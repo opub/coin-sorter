2d2bbba0c34ea668834291fab812ed4cc30204b0
tedoc <toconnor@gmail.com>
2024-07-06 22:15:37 -0400

    svelte + ts + favicon

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import logo from './assets/coins.png';
+</script>
+
+<main>
+  <div>
+    <img src={logo} class="logo" alt="Logo" />
+  </div>
+  <h1>Coin Sorter</h1>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+</style>
