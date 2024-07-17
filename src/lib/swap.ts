d5bc38f90ac69df9c818909484a9c83161d738d2
tedoc <toconnor@gmail.com>
2024-07-16 22:20:51 -0400

    added limit orders and param clean up

--- src/lib/swap.ts
+++ src/lib/swap.ts
@@ -1,6 +1,5 @@
 import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, VersionedTransaction } from '@solana/web3.js';
 import { getAccount, getAssociatedTokenAddressSync, getMint } from '@solana/spl-token';
-import { Wallet } from '@project-serum/anchor';
 import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
@@ -11,16 +10,16 @@ const connection = new Connection(RPC);
 
 // AyBgKL9XZa5FRehypY7tMzhhjcGJJZ3sHj13eA3Tu2Xm
 const walletPath = path.resolve('.secure/wallet.json');
-const wallet = new Wallet(Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync(walletPath, 'utf8')))));
+const wallet = Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync(walletPath, 'utf8'))));
 console.log('wallet', walletPath);
 
-function getTokenAddress(token: string): PublicKey {
-    const address = getAssociatedTokenAddressSync(new PublicKey(token), wallet.publicKey);
+function getTokenAddress(token: string, owner: PublicKey): PublicKey {
+    const address = getAssociatedTokenAddressSync(new PublicKey(token), owner);
     return address;
 }
 
-async function getTokenBalance(token: string): Promise<number> {
-    const address = getTokenAddress(token);
+async function getTokenBalance(token: string, owner: PublicKey): Promise<number> {
+    const address = getTokenAddress(token, owner);
     try {
         const account = await getAccount(connection, address);
         const mint = await getMint(connection, account.mint);
@@ -33,13 +32,13 @@ async function getTokenBalance(token: string): Promise<number> {
 }
 
 // https://station.jup.ag/docs/apis/swap-api#v6-api-reference
-async function buyToken(amountSOL: number, token: string, slippageBps: number = 500): Promise<void> {
+async function buyToken(inputMint: string, outputMint: string, amount: number, payer: Keypair, slippageBps: number = 500): Promise<void> {
     // get quote for swap
     const quoteResponse = await axios.get('https://quote-api.jup.ag/v6/quote', {
         params: {
-            inputMint: SOL,
-            outputMint: token,
-            amount: amountSOL * LAMPORTS_PER_SOL,
+            inputMint,
+            outputMint,
+            amount,
             slippageBps
         }
     }).then(response => response.data);
@@ -48,7 +47,7 @@ async function buyToken(amountSOL: number, token: string, slippageBps: number = 
     // see https://station.jup.ag/docs/apis/troubleshooting#transaction-confirmation-timeout
     const { swapTransaction } = await axios.post('https://quote-api.jup.ag/v6/swap', {
         quoteResponse,
-        userPublicKey: wallet.publicKey.toString(),
+        userPublicKey: payer.publicKey.toString(),
         wrapAndUnwrapSol: true,
         dynamicComputeUnitLimit: true,
         prioritizationFeeLamports: { autoMultiplier: 5 }
@@ -62,10 +61,44 @@ async function buyToken(amountSOL: number, token: string, slippageBps: number = 
     const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
     const transaction = VersionedTransaction.deserialize(swapTransactionBuf);
 
-    // sign the transaction
-    transaction.sign([wallet.payer]);
+    // sign, send and confirm
+    transaction.sign([payer]);
+    await sendAndConfirmTransaction(transaction);
+}
 
-    // send and confirm
+// https://station.jup.ag/docs/limit-order/limit-order-api
+async function createLimitOrder(inputMint: string, outputMint: string, inAmount: number, outAmount: number, payer: Keypair): Promise<void> {
+    // base key is used to generate a unique order id
+    const base = Keypair.generate();
+
+    // get serialized transaction for the order
+    const { tx } = await axios.post('https://jup.ag/api/limit/v1/createOrder', {
+        owner: payer.publicKey.toString(),
+        inAmount,
+        outAmount,
+        inputMint,
+        outputMint,
+        expiredAt: null, // new Date().valueOf() / 1000,
+        base: base.publicKey.toString(),
+        // https://station.jup.ag/docs/apis/adding-fees
+        // referralAccount: referral.publicKey.toString(),
+        // referralName: "Referral Name"
+    }, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.data);
+
+    // deserialize the transaction
+    const transactionBuf = Buffer.from(tx, 'base64');
+    const transaction = VersionedTransaction.deserialize(transactionBuf);
+
+    // sign, send and confirm
+    transaction.sign([payer, base]);
+    await sendAndConfirmTransaction(transaction);
+}
+
+async function sendAndConfirmTransaction(transaction: VersionedTransaction): Promise<void> {
     const { lastValidBlockHeight, blockhash } = await connection.getLatestBlockhash();
     const signature = await connection.sendTransaction(transaction, { maxRetries: 5 });
     const confirmation = await connection.confirmTransaction({
@@ -87,14 +120,14 @@ async function buyToken(amountSOL: number, token: string, slippageBps: number = 
 
     try {
         console.log('getting previous balance');
-        const previous = await getTokenBalance(token);
+        const previous = await getTokenBalance(token, wallet.publicKey);
         console.log('previous', previous);
 
         console.log('buying token');
-        await buyToken(amountSOL, token);
+        await buyToken(SOL, token, amountSOL * LAMPORTS_PER_SOL, wallet);
 
         console.log('getting current balance');
-        const current = await getTokenBalance(token);
+        const current = await getTokenBalance(token, wallet.publicKey);
         console.log('current', current);
 
         const bought = current - previous;
